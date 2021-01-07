28838ad11c48a397ae4c5cd953216951f6e458b0
atanasster <atanasster@gmail.com>
2021-01-07 03:37:04 -0800

    add design pages

--- src/TSButton/TSButton.stories.tsx
+++ src/TSButton/TSButton.stories.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Document, Example } from '@component-controls/core';
 import { TSButton } from './TSButton';
+import design_notes from '../media/design-notes.md';
+import image_screenshot from '../media/image-screenshot.jpg';
 
 export default {
   title: 'Typescript Button',
   component: TSButton,
+  plugins: {
+    figma: [
+      'https://www.figma.com/file/vgf0guEmC5IKtjHJKkRVSr/Button?node-id=0%3A1',
+    ],
+    notes: {
+      title: 'Design brief',
+      items: [design_notes],
+    },
+    images: {
+      title: 'Screenshots',
+      items: [image_screenshot],
+    },
+  },
 } as Document;
 
 export const main: Example = () => <TSButton>some text</TSButton>;
---
+++ src/media/design-notes.md
@@ -0,0 +1,23 @@
+#### Guidelines
+
+- A button component with some text, optional icon, and variants
+  1. **primary** for main, most important actions on secreen
+  2. **accent** for less-significant actions
+  3. **disabled** visually subdued
+  4. **success** to indicate a successful process
+  5. **warning** to indicate a process with potential issues
+  6. **error** to indicate a process with an error
+- The icons selected from [@primer/octicons-react](https://primer.style/octicons/)
+- Allow for padding, fontSize and icon side configuration
+
+#### Background colors
+
+- primary: #2F80ED
+- accent: #9B51E0
+- ...
+
+#### Padding variants
+
+- small: 5px 10px
+- medium: 10px 20px
+- large: 15px 30px
